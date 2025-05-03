fc41e06b384d2cf1adc26ee087a2727bed052186
Xurshid97 <xurshedrabo@gmail.com>
2025-05-03 13:16:00 +0200

    some updates

--- src/components/About.js
+++ src/components/About.js
@@ -8,21 +8,23 @@ function About() {
         backgroundColor: '#f8f9fa',
         padding: '20px',
       }}
-    ><h1 className="text-xl font-bold">About Page</h1>
-    <div>
-      This is a simple React application that demonstrates the following features:
-      <ul>
-        <li>Pages: Home, About, and Item Details</li>
-        <li>Data fetched from FakeStore API on the Home page</li>
-        <li>React Router for navigation</li>
-        <li>Home page fetching products from the FakeStore API</li>
-        <li>Item Detail page showing individual product details</li>
-      </ul>
-      <p>Feel free to explore the code and make modifications!</p>
-      <p>Created by [Khurshidjon]</p>
+    >
+      <h1 className="text-xl font-bold">About Page</h1>
+      <div>
+        This is a simple React application that demonstrates the following features:
+        <ul>
+          <li>Pages: Home, About, and Item Details</li>
+          <li>Data fetched from FakeStore API on the Home page</li>
+          <li>React Router for navigation</li>
+          <li>Home page fetching products from the FakeStore API</li>
+          <li>Item Detail page showing individual product details</li>
+        </ul>
+        <p>Feel free to explore the code and make modifications!</p>
+        <p>Created by [Khurshidjon]</p>
 
 
-      </div></div>;
+        </div>
+      </div>;
   }
 
   export default About;
